81996d6123beaeaae9ec8c044a9539f405ea4f9d
Brucknem <mbruckner94@gmail.com>
2020-07-16 15:19:16 +0200

    Added basic setup to implement compare trajectories

---
+++ bark_ml/library_wrappers/lib_tf2rl/trajectories_compare.py
@@ -0,0 +1,2 @@
+from bark_ml.library_wrappers.lib_tf2rl.load_expert_trajectories import *
+from bark_ml.library_wrappers.lib_tf2rl.load_save_utils import *
---
+++ bark_ml/tests/py_library_tf2rl_tests/trajectories_compare_test.py
@@ -0,0 +1,11 @@
+import unittest
+from bark_ml.library_wrappers.lib_tf2rl.trajectories_compare import *
+
+class TrajectoriesCompareTest(unittest.TestCase):
+
+    def test_test(self):
+        print('lol')
+
+
+if __name__ == '__main__':
+    unittest.main()
