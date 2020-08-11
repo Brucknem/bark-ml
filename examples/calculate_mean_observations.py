afa01e1a62237eed3e49bb198f7600c4eb4f91d8
Brucknem <mbruckner94@gmail.com>
2020-08-11 22:06:38 +0200

    Added normalized and not normalized compare

--- examples/calculate_mean_observations.py
+++ examples/calculate_mean_observations.py
@@ -1,6 +1,23 @@
-import numpy as np
+import os
+from pathlib import Path
+
 from absl import app
+from absl import flags
+
+# BARK imports
+from bark_project.bark.runtime.commons.parameters import ParameterServer
+from bark.runtime.viewer.matplotlib_viewer import MPViewer
+from bark.runtime.viewer.video_renderer import VideoRenderer
+
+# BARK-ML imports
+from bark_ml.environments.blueprints import ContinuousHighwayBlueprint, \
+  ContinuousMergingBlueprint, ContinuousIntersectionBlueprint
+from bark_ml.environments.single_agent_runtime import SingleAgentRuntime
+from bark_ml.library_wrappers.lib_tf2rl.tf2rl_wrapper import TF2RLWrapper
+from bark_ml.library_wrappers.lib_tf2rl.agents.gail_agent import BehaviorGAILAgent
+from bark_ml.library_wrappers.lib_tf2rl.runners.gail_runner import GAILRunner
 from bark_ml.library_wrappers.lib_tf2rl.load_expert_trajectories import load_expert_trajectories
+import numpy as np
 
 def calculate_mean(trajectories):
     """Calculates the mean of the observations and actions of the given trajectories.
@@ -27,21 +44,12 @@ def print_action(name, action):
     print('Ang: ', action[1])
     print('*' * 80)
 
-def run_configuration(argv):
-    """Loads the expert trajectories for the merging blueprint and the interaction dataset. Compares their mean actions and observations.
-
-    Args:
-        argv ([type]): [description]
-    """
-    merging, _, _ = load_expert_trajectories(
-        "../com_github_gail_4_bark_large_data_store/expert_trajectories/sac/merging_not_normalized",
-    ) 
+def compare_means(normalized, merging, interaction_dataset):
     merging_mean_observation, merging_mean_action = calculate_mean(merging)
-
-    interaction_dataset, _, _ = load_expert_trajectories(
-        "../com_github_gail_4_bark_large_data_store/expert_trajectories/interaction_dataset/DR_DEU_Merging_MT_v01_shifted/timestep_200_ms",
-    ) 
     interaction_dataset_mean_observation, interaction_dataset_mean_action = calculate_mean(interaction_dataset)
+
+    print('*' * 80)
+    print('Normalized: ', normalized)
 
     print('*' * 80)
     print('Observations')
@@ -56,6 +64,36 @@ def run_configuration(argv):
     print_action('Merging', merging_mean_action)
     print_action('Interaction Dataset', interaction_dataset_mean_action)
     print_action('Difference', interaction_dataset_mean_action - merging_mean_action)
+
+def run_configuration(argv):
+    """Loads the expert trajectories for the merging blueprint and the interaction dataset. Compares their mean actions and observations.
+
+    Args:
+        argv ([type]): [description]
+    """
+    merging, _, _ = load_expert_trajectories(
+        "../com_github_gail_4_bark_large_data_store/expert_trajectories/sac/merging_not_normalized",
+    ) 
+
+    interaction_dataset, _, _ = load_expert_trajectories(
+        "../com_github_gail_4_bark_large_data_store/expert_trajectories/interaction_dataset/DR_DEU_Merging_MT_v01_shifted/timestep_200_ms",
+    ) 
+    compare_means(False, merging, interaction_dataset)
+    
+    params = ParameterServer(filename="examples/example_params/gail_params.json")
+    bp = ContinuousMergingBlueprint(params,
+                                    number_of_senarios=2500,
+                                    random_seed=0)
+    env = SingleAgentRuntime(blueprint=bp,
+                            render=False)
+                            
+    merging, _, _ = load_expert_trajectories(
+        "../com_github_gail_4_bark_large_data_store/expert_trajectories/sac/merging_not_normalized",
+        normalize_features=True, env=env) 
+    interaction_dataset, _, _ = load_expert_trajectories(
+        "../com_github_gail_4_bark_large_data_store/expert_trajectories/interaction_dataset/DR_DEU_Merging_MT_v01_shifted/timestep_200_ms",
+        normalize_features=True, env=env) 
+    compare_means(True, merging, interaction_dataset)
 
 if __name__ == '__main__':
   app.run(run_configuration)
